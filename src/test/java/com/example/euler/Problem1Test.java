d500606bd85b4093171bcf5e60fa67e9a13294af
Neil McKeown <neilsimonmckeown@gmail.com>
2022-01-15 21:33:35 +0000

    More formatting

--- src/test/java/com/example/euler/Problem1Test.java
+++ src/test/java/com/example/euler/Problem1Test.java
@@ -5,14 +5,10 @@ import org.junit.jupiter.api.Test;
 
 class Problem1Test {
 
-    @Test
-    void solve() {
-        Assertions.assertEquals(
-                23,
-                new Problem1().solve(10, 3, 5));
+  @Test
+  void solve() {
+    Assertions.assertEquals(23, new Problem1().solve(10, 3, 5));
 
-        Assertions.assertEquals(
-                233168,
-                new Problem1().solve(1000, 3, 5));
-    }
+    Assertions.assertEquals(233168, new Problem1().solve(1000, 3, 5));
+  }
 }
