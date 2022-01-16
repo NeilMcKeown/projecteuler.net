998ddd08296552c32ffc425b0f784def5dfefc58
Neil McKeown <neilsimonmckeown@gmail.com>
2022-01-16 00:22:24 +0000

    Update Problem1 to use the Streams api

--- src/main/java/com/example/euler/Problem1.java
+++ src/main/java/com/example/euler/Problem1.java
@@ -1,25 +1,19 @@
 package com.example.euler;
 
+import java.util.stream.IntStream;
+
 /**
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
  * The sum of these multiples is 23.
  *
- * <p>Find the sum of all the multiples of 3 or 5 below 1000.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 public class Problem1 {
 
-  public int solve(int max, int... multiples) {
-    int sumOfMultiples = 0;
-
-    for (int i = 0; i < max; i++) {
-      for (int multiple : multiples) {
-        if (i % multiple == 0) {
-          sumOfMultiples += i;
-          break;
-        }
-      }
-    }
-
-    return sumOfMultiples;
+  public int solve(int max, int... factors) {
+    return IntStream.rangeClosed(0, max - 1)
+        .parallel()
+        .filter(i -> IntStream.of(factors).parallel().anyMatch(j -> i % j == 0))
+        .sum();
   }
 }
